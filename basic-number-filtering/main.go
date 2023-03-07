b8ec6a2e07b90f57625c65131f80be151a55cd53
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-03-07 22:42:30 +0530

    isOdd and isPrime function and testcase implementation

--- basic-number-filtering/main.go
+++ basic-number-filtering/main.go
@@ -23,3 +23,29 @@ func EvenNumbers(numberArray []int) []int {
 	return evenNumberArray
 
 }
+
+func isOdd(number int) bool {
+	return number%2 != 0
+}
+
+func OddNumbers(numberArray []int) []int {
+	var OddNumberArray []int
+	for _, num := range numberArray {
+		if isOdd(num) {
+			OddNumberArray = append(OddNumberArray, num)
+		}
+	}
+	return OddNumberArray
+}
+
+func isPrime(number int) bool {
+	if number < 2 {
+		return false
+	}
+	for i := 2; i*i <= number; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
+}
--- basic-number-filtering/main_test.go
+++ basic-number-filtering/main_test.go
@@ -38,3 +38,65 @@ func TestEvenNumbers(t *testing.T) {
 		t.Errorf("wanted: %v but got: %v", want, output)
 	}
 }
+
+func TestIsOdd(t *testing.T) {
+	//given
+	input_1 := 1
+	input_2 := 2
+	want_1 := true
+	want_2 := false
+
+	//when
+	got_1 := isOdd(input_1)
+	got_2 := isOdd(input_2)
+
+	//then
+	if !reflect.DeepEqual(got_1, want_1) {
+		t.Errorf("wanted: %v but got: %v", want_1, got_1)
+	}
+	if !reflect.DeepEqual(got_2, want_2) {
+		t.Errorf("wanted: %v but got: %v", want_2, got_2)
+	}
+
+}
+
+func TestOddNumbers(t *testing.T) {
+	// given
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	want := []int{1, 3, 5, 7, 9}
+
+	// when
+	got := OddNumbers(input)
+
+	//then
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+}
+
+func TestIsPrime(t *testing.T) {
+	//given
+	testCases := []struct {
+		n    int
+		want bool
+	}{
+		{2, true},
+		{3, true},
+		{5, true},
+		{7, true},
+		{11, true},
+		{4, false},
+		{6, false},
+		{8, false},
+		{9, false},
+		{10, false},
+	}
+
+	for _, tc := range testCases {
+		got := isPrime(tc.n)
+		want := tc.want
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("wanted: %v but got: %v", want, got)
+		}
+	}
+}
