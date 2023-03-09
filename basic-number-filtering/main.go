ee2e69ef1471455316bef63810067eff64bed27b
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-03-10 00:36:38 +0530

    Implemented story 5 and 6, any and all generic implementation for story 7 and 8 along with their test cases

--- basic-number-filtering/main.go
+++ basic-number-filtering/main.go
@@ -9,14 +9,18 @@ func main() {
 
 }
 
-func isEven(number int) bool {
+func IsOdd(number int) bool {
+	return number%2 != 0
+}
+
+func IsEven(number int) bool {
 	return number%2 == 0
 }
 
 func EvenNumbers(numberArray []int) []int {
 	var evenNumberArray []int
 	for _, num := range numberArray {
-		if isEven(num) {
+		if IsEven(num) {
 			evenNumberArray = append(evenNumberArray, num)
 		}
 	}
@@ -24,21 +28,11 @@ func EvenNumbers(numberArray []int) []int {
 
 }
 
-func isOdd(number int) bool {
-	return number%2 != 0
+func IsMultipleOf3(number int) bool {
+	return number%3 == 0
 }
 
-func OddNumbers(numberArray []int) []int {
-	var OddNumberArray []int
-	for _, num := range numberArray {
-		if isOdd(num) {
-			OddNumberArray = append(OddNumberArray, num)
-		}
-	}
-	return OddNumberArray
-}
-
-func isPrime(number int) bool {
+func IsPrime(number int) bool {
 	if number < 2 {
 		return false
 	}
@@ -48,4 +42,72 @@ func isPrime(number int) bool {
 		}
 	}
 	return true
+}
+
+func OddNumbers(numberArray []int) []int {
+	var OddNumberArray []int
+	for _, num := range numberArray {
+		if IsOdd(num) {
+			OddNumberArray = append(OddNumberArray, num)
+		}
+	}
+	return OddNumberArray
+}
+
+func OddPrime(numberArray []int) []int {
+	var OddPrimeNumbers []int
+	for _, num := range numberArray {
+		if IsOdd(num) && IsPrime(num) {
+			OddPrimeNumbers = append(OddPrimeNumbers, num)
+		}
+	}
+	return OddPrimeNumbers
+}
+
+func OddMultipleOf_3_GreaterThan_10(numberArray []int) []int {
+	var numbers []int
+	for _, num := range numberArray {
+		if IsOdd(num) && IsMultipleOf3(num) && num > 10 {
+			numbers = append(numbers, num)
+		}
+	}
+	return numbers
+}
+
+func All[T any](v T, functions ...func(T) bool) bool {
+	for _, f := range functions {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Any[T any](v T, functions ...func(T) bool) bool {
+	for _, f := range functions {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func filterAll(numberArray []int, conditions ...func(int) bool) []int {
+	var result []int
+	for _, num := range numberArray {
+		if All(num, conditions...) {
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func filterAny(numberArray []int, conditions ...func(int) bool) []int {
+	var result []int
+	for _, num := range numberArray {
+		if Any(num, conditions...) {
+			result = append(result, num)
+		}
+	}
+	return result
 }
--- basic-number-filtering/main_test.go
+++ basic-number-filtering/main_test.go
@@ -13,8 +13,8 @@ func TestIsEven(t *testing.T) {
 	want_2 := true
 
 	//when
-	got_1 := isEven(input_1)
-	got_2 := isEven(input_2)
+	got_1 := IsEven(input_1)
+	got_2 := IsEven(input_2)
 
 	//then
 	if !reflect.DeepEqual(got_1, want_1) {
@@ -47,8 +47,8 @@ func TestIsOdd(t *testing.T) {
 	want_2 := false
 
 	//when
-	got_1 := isOdd(input_1)
-	got_2 := isOdd(input_2)
+	got_1 := IsOdd(input_1)
+	got_2 := IsOdd(input_2)
 
 	//then
 	if !reflect.DeepEqual(got_1, want_1) {
@@ -93,7 +93,116 @@ func TestIsPrime(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		got := isPrime(tc.n)
+		got := IsPrime(tc.n)
+		want := tc.want
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("wanted: %v but got: %v", want, got)
+		}
+	}
+}
+
+func TestOddPrime(t *testing.T) {
+	//given
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	want := []int{3, 5, 7}
+
+	//got
+	got := OddPrime(input)
+
+	//then
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+
+}
+
+func TestOddMultipleOf_3_GreaterThan_10(t *testing.T) {
+	//given
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
+	want := []int{15, 21}
+
+	//got
+	got := OddMultipleOf_3_GreaterThan_10(input)
+
+	//then
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+
+}
+
+func TestFilerAll(t *testing.T) {
+	//given
+	IsGreaterThan5 := func(number int) bool { return number > 5 }
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	want := []int{9, 15}
+
+	//got
+	got := filterAll(input, IsOdd, IsGreaterThan5, IsMultipleOf3)
+
+	//then
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+
+}
+
+func TestFilterAll(t *testing.T) {
+	//given
+	IsGreaterThan5 := func(number int) bool { return number > 5 }
+	IsLessThan15 := func(number int) bool { return number < 15 }
+
+	testCases := []struct {
+		input     []int
+		functions []func(int) bool
+		want      []int
+	}{
+		{
+			input:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			functions: []func(int) bool{IsOdd, IsGreaterThan5, IsMultipleOf3},
+			want:      []int{9, 15},
+		},
+		{
+			input:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			functions: []func(int) bool{IsEven, IsLessThan15, IsMultipleOf3},
+			want:      []int{6, 12},
+		},
+	}
+
+	for _, tc := range testCases {
+		got := filterAll(tc.input, tc.functions...)
+		want := tc.want
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("wanted: %v but got: %v", want, got)
+		}
+	}
+}
+
+func TestFilterAny(t *testing.T) {
+	//given
+	IsGreaterThan15 := func(number int) bool { return number > 15 }
+	IsLessThan6 := func(number int) bool { return number < 6 }
+	IsMultipleOf5 := func(number int) bool { return number%5 == 0 }
+
+	testCases := []struct {
+		input     []int
+		functions []func(int) bool
+		want      []int
+	}{
+		{
+			input:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			functions: []func(int) bool{IsPrime, IsGreaterThan15, IsMultipleOf5},
+			want:      []int{2, 3, 5, 7, 10, 11, 13, 15, 16, 17, 18, 19, 20},
+		},
+		{
+			input:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			functions: []func(int) bool{IsLessThan6, IsMultipleOf3},
+			want:      []int{1, 2, 3, 4, 5, 6, 9, 12, 15, 18},
+		},
+	}
+
+	for _, tc := range testCases {
+		got := filterAny(tc.input, tc.functions...)
 		want := tc.want
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("wanted: %v but got: %v", want, got)
